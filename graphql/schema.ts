a52c935d1b1d4ecfb5d3750955a4b48bb55d788a
vitormiura <vitormiura3@gmail.com>
2022-08-12 11:27:41 -0300

    Init building api w/ graphql

---
+++ graphql/schema.ts
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server-micro'
+
+export const typeDefs = gql`
+    type Link {
+        id: String
+        title: String
+        description: String
+        url: String
+        imageUrl: String
+        users: [String]
+    }
+    type Query {
+        links: [Link]!
+    }
+`
