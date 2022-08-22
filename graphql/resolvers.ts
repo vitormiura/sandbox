e8cf712d9918e835b20ee74d2389ab4734f130e5
vitormiura <vitormiura3@gmail.com>
2022-08-22 07:53:23 -0300

    Edit project name in pkgs

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,32 +1,32 @@
 export const resolvers = {
-    Query: {
-      links: () => {
-        return [
-          {
-            category: 'Open Source',
-            description: 'Fullstack React framework',
-            id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqes',
-            imageUrl: 'https://nextjs.org/static/twitter-cards/home.jpg',
-            title: 'Next.js',
-            url: 'https://nextjs.org',
-          },
-          {
-            category: 'Open Source',
-            description: 'Next Generation ORM for TypeScript and JavaScript',
-            id: '2a3121b2-363b-4a4f-ad26-d6c35b41bade',
-            imageUrl: 'https://www.prisma.io/images/og-image.png',
-            title: 'Prisma',
-            url: 'https://prisma.io',
-          },
-          {
-            category: 'Open Source',
-            description: 'GraphQL implementation',
-            id: '2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78',
-            imageUrl: 'https://www.apollographql.com/apollo-home.jpg',
-            title: 'Apollo GraphQL',
-            url: 'https://apollographql.com',
-          },
-        ]
-      },
+  Query: {
+    links: () => {
+      return [
+        {
+          category: 'Open Source',
+          description: 'Fullstack React framework',
+          id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqes',
+          imageUrl: 'https://nextjs.org/static/twitter-cards/home.jpg',
+          title: 'Next.js',
+          url: 'https://nextjs.org',
+        },
+        {
+          category: 'Open Source',
+          description: 'Next Generation ORM for TypeScript and JavaScript',
+          id: '2a3121b2-363b-4a4f-ad26-d6c35b41bade',
+          imageUrl: 'https://www.prisma.io/images/og-image.png',
+          title: 'Prisma',
+          url: 'https://prisma.io',
+        },
+        {
+          category: 'Open Source',
+          description: 'GraphQL implementation',
+          id: '2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78',
+          imageUrl: 'https://www.apollographql.com/apollo-home.jpg',
+          title: 'Apollo GraphQL',
+          url: 'https://apollographql.com',
+        },
+      ]
     },
-  }
+  },
+}
