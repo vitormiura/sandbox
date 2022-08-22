70ee14e89cd2055f309a17435609273362529555
vitormiura <vitormiura3@gmail.com>
2022-08-22 09:13:38 -0300

    Add context and change resolvers

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,21 +1,22 @@
 import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
+import { createContext } from '../../graphql/context';
 import Cors from 'micro-cors'
 
 const cors = Cors()
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context: createContext, })
 
 const startServer = apolloServer.start()
 
 export default cors(async function handler(req, res) {
-    if(req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.end()
         return false
-    }    
+    }
     await startServer
-    
+
     await apolloServer.createHandler({
         path: '/api/graphql',
     })(req, res)
