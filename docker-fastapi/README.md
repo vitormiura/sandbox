a9d2ab1aa7f770921b9210a52c7f4ea974b26991
vitormiura <vitormiura3@gmail.com>
2022-08-28 22:19:19 -0300

    Increase ports

--- docker-fastapi/README.md
+++ docker-fastapi/README.md
@@ -1,4 +1,4 @@
 # useful commands
 
 - docker build -t python-fastapi .
-- docker run python-fastapi
+- docker run -p 8000:8000 python-fastapi
--- docker-fastapi/app/main.py
+++ docker-fastapi/app/main.py
@@ -15,4 +15,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run(app, port=8000, host="0.0.0.0")
