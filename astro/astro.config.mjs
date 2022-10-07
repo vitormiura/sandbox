2a23cdfef3218c5cd33f02e784ee0c75ed0d1ab1
vitormiura <vitormiura3@gmail.com>
2022-10-07 08:14:25 -0300

    astro components add and blog post poc

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import lit from "@astrojs/lit";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations:[react()],
+  integrations: [react(), lit(), svelte()]
 });
