d1f7f2de9a5808447b75fe9a3081b800fdd4e9ac
vitormiura <vitormiura3@gmail.com>
2023-01-29 22:12:18 -0300

    trpc(mky): add query to retrieve all mkys and get all mutation

--- trpc/mky-catalog/client/src/App.tsx
+++ trpc/mky-catalog/client/src/App.tsx
@@ -4,30 +4,59 @@ import { api } from "./utils/trpc";
 
 function App() {
   const queryClient = useQueryClient();
-  const animesQuery = useQuery(['allAnimes'], () => api.allAnime.query())  
-  const createAnimeMutation = useMutation(["createAnime"], api.createAnime.mutate)
+  const monkeysQuery = useQuery(["allMky"], () => api.allMky.query());
+  const createMonkeyMutation = useMutation(
+    ["createMonkey"],
+    api.createMonkey.mutate
+  );
 
-  const [name, setName] = useState("")
-  const [year, setYear] = useState("")
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [specie, setSpecie] = useState("");
+  const [photoUrl, setphotoUrl] = useState("");
 
-  if (animesQuery.isLoading) return <div>Loading...</div>;
-  if (animesQuery.isError) return <div>ERROR!</div>;
+  if (monkeysQuery.isLoading) return <div>Loading...</div>;
+  if (monkeysQuery.isError) return <div>ERROR!</div>;
 
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <p>{JSON.stringify(animesQuery.data?.allAnimes)}</p>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        createAnimeMutation.mutate({ name, year });
-        queryClient.invalidateQueries('allAnimes');
-      }}>
-        <input type="text" placeholder="Naruto" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="text" placeholder="2003" value={year} onChange={(e) => setYear(e.target.value)} />
-        <button type="submit">Add anime</button>
+      <p>{JSON.stringify(monkeysQuery.data?.allMky)}</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createMonkeyMutation.mutate({ name, age, specie, photoUrl });
+          queryClient.invalidateQueries("allMonkeys");
+        }}
+      >
+        <input
+          type="text"
+          placeholder="czar"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="69"
+          value={age}
+          onChange={(e) => setAge(e.target.valueAsNumber)}
+        />
+        <input
+          type="text"
+          placeholder="babuino"
+          value={specie}
+          onChange={(e) => setSpecie(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="bololohaha"
+          value={photoUrl}
+          onChange={(e) => setphotoUrl(e.target.value)}
+        />
+        <button type="submit">Add monkey</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
