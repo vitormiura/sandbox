21364c2c92d651277251b341148a110401bd8ff9
vitormiura <vitormiura3@gmail.com>
2023-05-19 09:17:14 -0300

    test init rust w/ nix env

---
+++ rust/yo/target/debug/deps/yo-c678e2076c396dc6.d
@@ -0,0 +1,5 @@
+/home/miura/ws/sandbox/rust/yo/target/debug/deps/yo-c678e2076c396dc6: src/main.rs
+
+/home/miura/ws/sandbox/rust/yo/target/debug/deps/yo-c678e2076c396dc6.d: src/main.rs
+
+src/main.rs:
---
+++ rust/yo/target/debug/yo.d
@@ -0,0 +1,1 @@
+/home/miura/ws/sandbox/rust/yo/target/debug/yo: /home/miura/ws/sandbox/rust/yo/src/main.rs
