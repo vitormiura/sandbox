05e23877cc1f5173d78b8e56f002646123908cf2
vitormiura <vitormiura3@gmail.com>
2023-06-15 15:10:16 -0300

    rust variables exercise done

---
+++ rust/ultimate_rust_crash_course/exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,9 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles - ready);
+}
