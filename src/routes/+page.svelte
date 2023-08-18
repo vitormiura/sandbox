9aeaeeed9a22aab297110db7134beb0d68533078
vitormiura <vitormiura3@gmail.com>
2023-08-18 09:10:13 -0300

    client n server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types'
+
+	export let data: PageData
+
+	$: ({ articles } = data)
+</script>
+
+<div class="grid">
+	<div>
+		<h2>Articles:</h2>
+		{#each articles as article}
+			<article>
+				<header>{article.title}</header>
+				<p>
+					{article.content}
+				</p>
+				<form action="?/deleteArticle&id={article.id}" method="POST">
+					<button type="submit" class="outline secondary">Delete Article</button>
+				</form>
+				<a href="/{article.id}" role="button" class="outline constrast" style="width: 100%;"
+					>Edit Article</a
+				>
+			</article>
+		{/each}
+	</div>
+	<form action="?/createArticle" method="POST">
+		<h3>New Article</h3>
+		<label for="title"> Title </label>
+		<input type="text" id="title" name="title" />
+		<label for="title"> Title </label>
+		<textarea id="content" name="content" rows={5} />
+		<button type="submit">Add Article</button>
+	</form>
+</div>
