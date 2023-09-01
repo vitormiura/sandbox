04c8acaba16c5c4d8e096cc7d219599ec1c7045c
vitormiura <vitormiura3@gmail.com>
2023-09-01 08:39:35 -0300

    add picocss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '@picocss/pico';
+</script>
+
+<div class="container">
+	<hgroup>
+		<h1>
+			<a href="/">miurinha´s blog</a>
+		</h1>
+
+		<h2>svelte clean asf</h2>
+	</hgroup>
+	<slot />
+</div>
