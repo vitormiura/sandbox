077f7ccf429179915b03a3b9aad1d6a3cce8ddf8
Miura <vitormiura3@gmail.com>
2023-11-29 17:38:17 +0000

    add .gitgnore and init web server

--- rust/todo-actix/src/main.rs
+++ rust/todo-actix/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+
+struct AppState {
+    todolist_entries: Mutex<Vec<TodolistEntry>>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TodolistEntry {
+    id: i32,
+    date: i64,
+    title: String
+}
+
+#[get("/")]
+async fn index() -> String{
+    "abe viado".to_string()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    let app_data = web::Data::new(AppState{
+        todolist_entries: Mutex::new(vec![])
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_data.clone())
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
